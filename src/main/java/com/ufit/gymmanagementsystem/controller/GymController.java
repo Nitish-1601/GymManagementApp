196d5fd5a1269a80f283dfaa0be8df80d54f55ea
Nitish-1601 <n.sharma8427@gmail.com>
2020-11-25 18:04:10 +0530

    Added Spring Security i.e form based

--- src/main/java/com/ufit/gymmanagementsystem/controller/GymController.java
+++ src/main/java/com/ufit/gymmanagementsystem/controller/GymController.java
@@ -7,15 +7,17 @@ import com.ufit.gymmanagementsystem.repo.UserRegistrationRepo;
 import com.ufit.gymmanagementsystem.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import sun.security.util.Password;
 
 import java.util.Optional;
 
 
 @Controller
 public class GymController {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private UserRegistrationRepo userRegistrationRepo;
     @Autowired
@@ -25,44 +27,63 @@ public class GymController {
     @Autowired
     private UserServices userServices;
 
-    @GetMapping("/gym")
+    @GetMapping("/")
     public String home() {
-        return "gym.html";
+        return "home";
+    }
+
+    @GetMapping("gym")
+    public String gym() {
+        return "gym";
     }
 
     @GetMapping("/register")
     public String register() {
-        return "registration.html";
+        return "registration";
+    }
+    @GetMapping("/diet")
+    public String diet() {
+        return "diet";
+    }
+    @GetMapping("/login_fail")
+    public String login_fail() {
+        return "login_fail";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login.html";
+        return "login";
     }
 
 
     @PostMapping("/save-user")
-    public ResponseEntity<?> insertToDb(UserRegistration userRegistration ,UserPersonalInfo userPersonalInfo, UserCredentials userCredentials) {
+    public String insertToDb(UserRegistration userRegistration ,UserPersonalInfo userPersonalInfo, UserCredentials userCredentials) {
+
+        String encodedPassword = bCryptPasswordEncoder.encode(userRegistration.getPassword());
+        userRegistration.setPassword(encodedPassword);
+        userCredentials.setPassword(encodedPassword);
         userRegistrationRepo.save(userRegistration);
         userPersonalInfoRepo.save(userPersonalInfo);
         userCredentialsRepo.save(userCredentials);
-        return ResponseEntity.ok("Hello "+ userPersonalInfo.getFirstName()+" you have been registered");
+        return "registerSuccessful";
 
     }
 
-    @PostMapping("/verify")
-    public ResponseEntity<?> fetchAndVerify(LoginVerifier loginVerifier ) {
-        Optional<UserCredentials> validate = userServices.findId(loginVerifier.getEmail());
-        if(validate.isPresent()) {
-            if (userServices.getById(loginVerifier.getEmail()).getPassword().equals(loginVerifier.getPassword()))
-                return ResponseEntity.ok("hi!! you are logged in");
-            else
-                return ResponseEntity.ok("Username or Password not correct");
-        }
-        else
-            return ResponseEntity.ok("Username or Password not correct");
 
+     //    this is manual verifier not using spring security
 
-    }
+//    @PostMapping("/verify")
+//    public String fetchAndVerify(LoginVerifier loginVerifier ) {
+//        Optional<UserCredentials> optionalUserCredentials = userServices.findId(loginVerifier.getEmail());
+//        if(!optionalUserCredentials.isPresent()) {
+//            return "login_fail";
+//        }
+//        UserCredentials userCredentials = optionalUserCredentials.get();
+//
+//        if (!bCryptPasswordEncoder.matches(loginVerifier.getPassword(),userCredentials.getPassword())){
+//            return "login_fail";
+//        }
+//        return "/gym";
+//    }
 
 }
--- src/main/java/com/ufit/gymmanagementsystem/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ufit/gymmanagementsystem/security/ApplicationSecurityConfig.java
@@ -1,25 +1,64 @@
-//package com.ufit.gymmanagementsystem.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/","/gym/*", "/login","/register","/save-credentials","/save-user","/img/*", "/js/*", "/style.css" )
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic().disable();
-//
-//    }
-//}
+package com.ufit.gymmanagementsystem.security;
+
+import com.ufit.gymmanagementsystem.service.UserServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserServices();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/diet", "/gym")
+                .authenticated()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                .permitAll()
+                .loginPage("/login")
+                .permitAll()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/gym")
+                .failureUrl("/login_fail")
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+    }
+}
